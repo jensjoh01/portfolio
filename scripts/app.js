7ab8dee2994303e349731584c044cf7a6d8b5502
jensjoh01 <johnnyj2439@gmail.com>
2017-08-17 20:51:54 -0700

    image carousel

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 'use strict';
+$(function() {
+  var interval;
+  var count = 0;
+
+  function imageSlider() {
+    var width = 720;
+    interval = setInterval(function(){
+      $('.hero-images').animate({'margin-left': '-=' + width}, 1000, function(){
+        count++
+        if(count === 3) {
+          $('.hero-images').css('margin-left',0)
+          count = 0;
+        }
+      });
+    },3000);
+    };
+    imageSlider();
+  });
