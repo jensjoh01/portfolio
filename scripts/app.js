5f6c526f8e57e452680b2402acaafd5b5f050d36
jensjoh01 <johnnyj2439@gmail.com>
2017-08-19 12:05:07 -0700

    slider working and tab navigations

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,35 @@
 'use strict';
 $(function() {
+
   var interval;
   var count = 0;
+  var width = 1020;
+
+  $('main section').hide();
+
+  $('header nav ul li a').on('click', function() {
+    $('.img-container').hide();
+    $('main section').hide();
+    var $viewSection = $(this).data('tab');
+    $('#' + $viewSection).fadeIn(500);
+  });
+
+  $('header h1').on('click', function() {
+    $('main section').hide();
+    $('.img-container').fadeIn(500);
+  });
 
   function imageSlider() {
-    var width = 720;
     interval = setInterval(function(){
       $('.hero-images').animate({'margin-left': '-=' + width}, 1000, function(){
-        count++
+        count++;
         if(count === 3) {
-          $('.hero-images').css('margin-left',0)
+          $('.hero-images').css('margin-left',0);
           count = 0;
         }
       });
-    },3000);
-    };
-    imageSlider();
-  });
+    },4000);
+  };
+
+  imageSlider();
+});
