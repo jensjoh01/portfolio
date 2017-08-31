5c17453118de4571d1cb32b9c273111e2f07c1a5
jensjoh01 <johnnyj2439@gmail.com>
2017-08-31 09:38:42 -0700

    added semicolons in projectView

--- projectView.js
+++ projectView.js
@@ -13,13 +13,13 @@ var app = app || {};
     $('.img-container').hide();
     $('main section').hide();
     $('#about').fadeIn(500);
-  }
+  };
 
   experienceController.init = function() {
     $('.img-container').hide();
     $('main section').hide();
     $('#experience').fadeIn(500);
-  }
+  };
 
   projectsController.init = function() {
     $('.img-container').hide();
@@ -27,21 +27,21 @@ var app = app || {};
     $('#projects').fadeIn(500);
     $('#algorithm-teacher').fadeIn(500);
     $('#urban-sherpa').fadeIn(500);
-  }
+  };
 
   homeController.init = function() {
     $('main section').fadeIn(500);
     $('.img-container').fadeIn(500);
-  }
+  };
 
   contactController.init = function() {
     $('.img-container').hide();
     $('main section').hide();
-  }
+  };
 
   module.projectsController = projectsController;
   module.experienceController = experienceController;
   module.aboutController = aboutController;
   module.contactController = contactController;
   module.homeController = homeController;
-})(app)
+})(app);
