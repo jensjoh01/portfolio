240d5d95589fb3e8d06bd5e7ff33f7bf7b24c0cf
jensjoh01 <johnnyj2439@gmail.com>
2018-01-14 23:05:42 -0800

    working on flask app

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_view():
+    """."""
+    return 'Home!'
+
+
+@app.route('/projects/')
+def projects_view():
+    """."""
+    return 'projects are here'
+
+
+@app.route('/about')
+def about_view():
+    """."""
+    return 'this is the about me view'
